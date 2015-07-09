0905d7869f8fb92deeb1c590d6b2c93958d61e27
Miles Peele <mbpeele@email.wm.edu>
2015-07-08 20:39:56 -0400

    FAB button now animates out/in on gesture detected

--- app/src/main/java/milespeele/canvas/view/ViewFabDrawerBehavior.java
+++ app/src/main/java/milespeele/canvas/view/ViewFabDrawerBehavior.java
@@ -5,8 +5,10 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPropertyAnimatorListener;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.animation.BounceInterpolator;
 import android.view.animation.Interpolator;
 
@@ -16,6 +18,7 @@ import android.view.animation.Interpolator;
 public class ViewFabDrawerBehavior extends FloatingActionButton.Behavior {
 
     private boolean mIsAnimatingOut;
+    private boolean mIsAnimatingIn;
 
     private final static Interpolator INTERPOLATOR = new BounceInterpolator();
 
@@ -29,45 +32,64 @@ public class ViewFabDrawerBehavior extends FloatingActionButton.Behavior {
         switch (action) {
             case MotionEvent.ACTION_MOVE:
                 animateOut(child);
+                break;
             case MotionEvent.ACTION_UP:
                 animateIn(child);
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                animateIn(child);
+                break;
         }
         return false;
     }
 
     private void animateOut(final FloatingActionButton button) {
-        ViewCompat.animate(button).scaleX(0).scaleY(0).alpha(0)
-                .setInterpolator(INTERPOLATOR)
-                .withLayer()
-                .start();
-//        ViewCompat.animate(button).scaleX(0.0F).scaleY(0.0F).alpha(0.0F).setInterpolator(
-//                new AccelerateDecelerateInterpolator()).withLayer()
-//                .setListener(new ViewPropertyAnimatorListener() {
-//                    public void onAnimationStart(View view) {
-//                        ViewFabDrawerBehavior.this.mIsAnimatingOut = true;
-//                    }
-//
-//                    public void onAnimationCancel(View view) {
-//                        ViewFabDrawerBehavior.this.mIsAnimatingOut = false;
-//                    }
-//
-//                    public void onAnimationEnd(View view) {
-//                        ViewFabDrawerBehavior.this.mIsAnimatingOut = false;
-//                        view.setVisibility(View.GONE);
-//                    }
-//                }).start();
+        if (!mIsAnimatingOut) {
+            ViewCompat.animate(button).scaleX(0).scaleY(0).alpha(0)
+                    .setInterpolator(INTERPOLATOR)
+                    .withLayer()
+                    .setListener(new ViewPropertyAnimatorListener() {
+                        @Override
+                        public void onAnimationStart(View view) {
+                            ViewFabDrawerBehavior.this.mIsAnimatingOut = true;
+                        }
+
+                        @Override
+                        public void onAnimationEnd(View view) {
+                            ViewFabDrawerBehavior.this.mIsAnimatingOut = false;
+                        }
+
+                        @Override
+                        public void onAnimationCancel(View view) {
+                            ViewFabDrawerBehavior.this.mIsAnimatingOut = false;
+                        }
+                    })
+                    .start();
+        }
     }
 
     private void animateIn(FloatingActionButton button) {
-//        button.setVisibility(View.VISIBLE);
-//        ViewCompat.animate(button).scaleX(1.0F).scaleY(1.0F).alpha(1.0F)
-//                .setInterpolator(new AccelerateDecelerateInterpolator())
-//                .withLayer().setListener(null)
-//                .start();
-//        ViewCompat.animate(button).scaleX(1).scaleY(1).alpha(1)
-//                .setInterpolator(INTERPOLATOR)
-//                .withLayer()
-//                .start();
+        button.setVisibility(View.VISIBLE);
+        ViewCompat.animate(button).scaleX(1.0F).scaleY(1.0F).alpha(1.0F)
+                .setInterpolator(INTERPOLATOR)
+                .withLayer()
+                .setListener(new ViewPropertyAnimatorListener() {
+                    @Override
+                    public void onAnimationStart(View view) {
+                        ViewFabDrawerBehavior.this.mIsAnimatingIn = true;
+                    }
+
+                    @Override
+                    public void onAnimationEnd(View view) {
+                        ViewFabDrawerBehavior.this.mIsAnimatingIn = false;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(View view) {
+                        ViewFabDrawerBehavior.this.mIsAnimatingIn = false;
+                    }
+                })
+                .start();
     }
 
 }
