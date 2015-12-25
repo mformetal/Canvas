3c3742bfb3394959d399397be1ff8c6ef2b9c926
mbpeele <mbpeele@email.wm.edu>
2015-12-24 20:32:34 -0500

    Got BrushPickerLayout working again

---
+++ app/src/main/java/milespeele/canvas/adapter/AdapterBrushPicker.java
@@ -0,0 +1,7 @@
+package milespeele.canvas.adapter;
+
+/**
+ * Created by mbpeele on 12/24/15.
+ */
+public class AdapterBrushPicker {
+}
---
+++ app/src/main/java/milespeele/canvas/view/ViewBrushPickerLayout.java
@@ -0,0 +1,7 @@
+package milespeele.canvas.view;
+
+/**
+ * Created by mbpeele on 12/24/15.
+ */
+public class ViewBrushPickerLayout {
+}
